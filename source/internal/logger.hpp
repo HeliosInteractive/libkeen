698af7631d8634986384e3b3671cf5dd28473c73
Sepehr Laal <sepehr.laal@gmail.com>
2016-04-16 12:16:26 -0700

    checked in my simple thread-safe logger form Sodium

---
+++ source/internal/logger.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <sstream>
+#include <memory>
+#include <string>
+#include <mutex>
+
+namespace libkeen {
+namespace internal {
+
+class Logger
+{
+public:
+    Logger              (const std::string& type);
+    void                log(const std::string& message);
+
+private:
+    std::mutex          mMutex;
+    const std::string   mType;
+};
+
+namespace loggers
+{
+
+std::shared_ptr<Logger> debug();
+std::shared_ptr<Logger> error();
+std::shared_ptr<Logger> warn();
+std::shared_ptr<Logger> info();
+
+#define LOG_DEBUG(stream)   { std::stringstream ss; ss << stream << " [" << __FILE__ << "@ln" << __LINE__ << "]"; libkeen::internal::loggers::debug()->log(ss.str()); }
+#define LOG_ERROR(stream)   { std::stringstream ss; ss << stream << " [" << __FILE__ << "@ln" << __LINE__ << "]"; libkeen::internal::loggers::error()->log(ss.str()); }
+#define LOG_WARN(stream)    { std::stringstream ss; ss << stream << " [" << __FILE__ << "@ln" << __LINE__ << "]"; libkeen::internal::loggers::warn()->log(ss.str()); }
+#define LOG_INFO(stream)    { std::stringstream ss; ss << stream << " [" << __FILE__ << "@ln" << __LINE__ << "]"; libkeen::internal::loggers::info()->log(ss.str()); }
+
+}
+
+}}
