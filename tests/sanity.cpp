586bf99f80fe17f567bfbb28c3949a076ebf1fff
Sepehr Laal <sepehr.laal@gmail.com>
2016-04-16 11:31:15 -0700

    added version API to sanity checks (to test lib loading and etc.)

--- tests/sanity.cpp
+++ tests/sanity.cpp
@@ -1,5 +1,10 @@
 #include "common.hpp"
+#include "keen.hpp"
 
-TEST_CASE("Sanity Check", "[sanity]") {
+TEST_CASE("Sanity Check", "[sanity]")
+{
     REQUIRE(1 == 1);
+    REQUIRE(libkeen::version::getMajor() >= 0);
+    REQUIRE(libkeen::version::getMinor() >= 0);
+    REQUIRE(libkeen::version::getPatch() >= 0);
 }
