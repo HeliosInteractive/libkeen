84b4f9d6ad311cb32f37201031eff39c8c5ec2f3
Sepehr Laal <sepehr.laal@gmail.com>
2016-04-16 11:30:26 -0700

    checked in version API

---
+++ source/keen/version.cpp
@@ -0,0 +1,21 @@
+#include "keen/version.hpp"
+
+namespace libkeen {
+namespace version {
+
+int getMajor()
+{
+    return MAJOR;
+}
+
+int getMinor()
+{
+    return MINOR;
+}
+
+int getPatch()
+{
+    return PATCH;
+}
+
+}}
