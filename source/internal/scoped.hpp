13cdc77143aa89a691f96de82d026671e968d99e
Sepehr Laal <sepehr.laal@gmail.com>
2016-04-18 11:05:26 -0700

    introduced a scoped pointer class to bind C-style pointers to C++ scopes

---
+++ source/internal/scoped.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+namespace libkeen {
+namespace internal {
+
+template<typename T>
+class Scoped
+{
+public:
+    Scoped(T*& pointer);
+    void onScopeEnd();
+    ~Scoped();
+private:
+    T*& mPointer;
+};
+
+template<typename T>
+Scoped<T>::Scoped(T*& pointer)
+    : mPointer(pointer)
+{}
+
+template<typename T>
+void Scoped<T>::onScopeEnd()
+{
+    delete mPointer;
+}
+
+template<typename T>
+Scoped<T>::~Scoped()
+{
+    onScopeEnd();
+    mPointer = nullptr;
+}
+
+}}
