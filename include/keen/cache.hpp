90c16a8eb5b93a5fe423b7cb35b2acfe6497812e
Sepehr Laal <sepehr.laal@gmail.com>
2016-04-18 11:30:15 -0700

    a basic cache implementation checked in

--- include/keen/cache.hpp
+++ include/keen/cache.hpp
@@ -16,9 +16,9 @@ class Cache
 public:
     ~Cache();
     static Sqlite3Ref   ref();
-    void                push(const std::string& record);
-    void                pop(const std::vector<std::string>& records, int count);
-    void                remove(const std::string& record);
+    void                push(const std::string& name, const std::string& event);
+    void                pop(std::vector<std::pair<std::string, std::string>>& records, unsigned count);
+    void                remove(const std::string& name, const std::string& event);
 
 private:
     Cache();
--- source/keen/cache.cpp
+++ source/keen/cache.cpp
@@ -1,6 +1,7 @@
 #include "keen/cache.hpp"
 #include "logger.hpp"
-#include <sqlite3.h>
+#include "scoped.hpp"
+#include "sqlite3.h"
 
 namespace libkeen {
 namespace internal {
@@ -36,19 +37,75 @@ Cache::~Cache()
     mConnection = nullptr;
 }
 
-void Cache::push(const std::string& record)
+void Cache::push(const std::string& name, const std::string& event)
 {
     if (!mConnection) return;
+    
+    sqlite3_stmt *stmt = nullptr;
+    Scoped<sqlite3_stmt> scope_bound_stmt(stmt);
+
+    std::stringstream ss;
+    ss << "INSERT OR IGNORE INTO cache (name, event) VALUES ('" << name << "','" << event << "')";
+
+    if (sqlite3_prepare_v2(mConnection, ss.str().c_str(), -1, &stmt, nullptr) != SQLITE_OK)
+    {
+        LOG_ERROR("Unable to prepare the statement.");
+        return;
+    }
+
+    if (sqlite3_step(stmt) != SQLITE_DONE)
+    {
+        LOG_ERROR("Unable to step the statement.");
+        return;
+    }
 }
 
-void Cache::pop(const std::vector<std::string>& records, int count)
+void Cache::pop(std::vector<std::pair<std::string, std::string>>& records, unsigned count)
 {
     if (!mConnection) return;
+    if (!records.empty()) records.clear();
+
+    sqlite3_stmt *stmt = nullptr;
+    Scoped<sqlite3_stmt> scope_bound_stmt(stmt);
+
+    std::stringstream ss;
+    ss << "SELECT name, event FROM cache LIMIT " << count;
+
+    if (sqlite3_prepare_v2(mConnection, ss.str().c_str(), -1, &stmt, nullptr) != SQLITE_OK)
+    {
+        LOG_ERROR("Unable to prepare the statement.");
+        return;
+    }
+
+    while (sqlite3_step(stmt) == SQLITE_ROW)
+    {
+        std::string name{ reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0)) };
+        std::string data{ reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1)) };
+        records.push_back(std::make_pair(name, data));
+    }
 }
 
-void Cache::remove(const std::string& record)
+void Cache::remove(const std::string& name, const std::string& event)
 {
     if (!mConnection) return;
+
+    sqlite3_stmt *stmt = nullptr;
+    Scoped<sqlite3_stmt> scope_bound_stmt(stmt);
+
+    std::stringstream ss;
+    ss << "DELETE FROM cache WHERE name='" << name << "' AND event='" << event << "'";
+
+    if (sqlite3_prepare_v2(mConnection, ss.str().c_str(), -1, &stmt, nullptr) != SQLITE_OK)
+    {
+        LOG_ERROR("Unable to prepare the statement.");
+        return;
+    }
+
+    if (sqlite3_step(stmt) != SQLITE_DONE)
+    {
+        LOG_ERROR("Unable to step the statement.");
+        return;
+    }
 }
 
 }}
