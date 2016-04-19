aabc993cb4a71bbc71def0cc5c3e4c31f595493e
Sepehr Laal <sepehr.laal@gmail.com>
2016-04-19 15:40:16 -0700

    added docs to Curl class

--- source/internal/curl.hpp
+++ source/internal/curl.hpp
@@ -9,10 +9,17 @@
 namespace libkeen {
 namespace internal {
 
+/*!
+ * @class Curl
+ * @brief Handles interfacing with cURL.
+ * @note  Realistically only one instance of this class
+ *        Exists at a time and Core owns it.
+ */
 class Curl
 {
 public:
     Curl();
+    //! send a application/json request to given url using cURL
     bool    sendEvent(const std::string& url, const std::string& json);
 
 private:
