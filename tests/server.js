4e78c903e4daba80ec364ac9e0c13d56e6697555
Sepehr Laal <sepehr.laal@gmail.com>
2016-04-20 12:19:08 -0700

    Added a mock Node server to test our network calls

---
+++ tests/server.js
@@ -0,0 +1,28 @@
+var http = require('http');
+var count = 0;
+
+http.createServer(function(request, response)
+{
+  console.log(request.url);
+  if (request.url == '/pass') {
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.end("OK");
+	count++;
+	console.log(count);
+  } else if (request.url == '/count') {
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.end("" + count);
+  } else if (request.url == '/clear') {
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.end("CLEAR");
+	count = 0;
+  } else if (request.url == '/fail') {
+	response.writeHead(400);
+	response.end("FAIL");
+  } else if (request.url == '/shutdown') {
+	process.exit(0);
+  } else {
+	response.writeHead(403);
+	response.end("NOPE");
+  }
+}).listen(8080);
